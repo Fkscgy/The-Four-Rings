a6028ee10bee744b7f5008544dd726e335ae1c0a
Fkscgy <idordestru@gmail.com>
2021-05-24 15:18:34 -0300

    remocao itens inuteis

--- Assets/Scripts/MeteoroDaPaixao.cs
+++ Assets/Scripts/MeteoroDaPaixao.cs
@@ -4,23 +4,26 @@ using UnityEngine;
 
 public class MeteoroDaPaixao : MonoBehaviour
 {
-    bool aaaa = false;
     [SerializeField]
     GameObject prefab;
     float varX;
-    
+
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.U) && PlayerOneBehaviour.ultimateIsReady)
         {
             StartCoroutine(example());
+            PlayerOneBehaviour.ultimateIsReady = false;
+        } else if(Input.GetKeyDown(KeyCode.U) && !PlayerOneBehaviour.ultimateIsReady)
+        {
+            Debug.Log("A ultimate ainda não esta pronta");
         }
     }
     IEnumerator example()
     {
         for(int i =0;i<25;i++)
         {
-            varX =Random.Range(-25f,15f);
+            varX = Random.Range(-25f,15f);
             Instantiate(prefab,new Vector3(varX,4.4f,0f),Quaternion.Euler(0f,0f,-45f));
             yield return new WaitForSeconds(Random.Range(0.05f,0.1f));
         }
